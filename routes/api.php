390bdd4b3a90168a9bc3d75a20ea32e794e41db2
Bishrul Haq <withmybishrul@gmail.com>
2019-04-29 11:53:07 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,6 @@ use Illuminate\Http\Request;
 
 Route::get('/data', 'DataController@index')->name('path.index');
 Route::post('/data', 'DataController@store')->name('path.store');
-Route::get('/data/{data}', 'DataController@show')->name('path.show');
+Route::get('/data/{id}', 'DataController@show')->name('path.show');
 Route::put('/data/{data}', 'DataController@update')->name('path.update');
 Route::delete('/data/{data}', 'DataController@destroy')->name('path.destroy');
